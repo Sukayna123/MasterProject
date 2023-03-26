af66be768838952bfd2356e9af00693c3f7d45ba
sukayna <shalashsukayna@gmail.com>
2023-03-26 13:09:13 -0400

    11

---
+++ src/test/java/logout/pages/logoutNavigation.java
@@ -0,0 +1,100 @@
+package logout.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class logoutNavigation {
+
+    @FindBy()
+    public WebElement myAccount;
+
+    @FindBy()
+    public WebElement loginButtonHomePage;
+
+    @FindBy()
+    public WebElement returningCustomerEmailAddress;
+
+    @FindBy()
+    public WebElement returningCustomerPassword;
+
+    @FindBy()
+    public WebElement returningCustomerLoginButton;
+
+    @FindBy()
+    public WebElement
+
+    @FindBy()
+    public WebElement
+
+
+    @FindBy()
+    public WebElement
+
+
+    @FindBy()
+    public WebElement
+
+
+    @FindBy()
+    public WebElement
+
+
+    @FindBy()
+    public WebElement
+
+
+    @FindBy()
+    public WebElement
+
+    @FindBy()
+    public WebElement
+
+
+    @FindBy()
+    public WebElement
+
+    @FindBy()
+    public WebElement
+
+
+    @FindBy()
+    public WebElement
+
+
+    @FindBy()
+    public WebElement
+
+
+    @FindBy()
+    public WebElement
+
+
+    @FindBy()
+    public WebElement
+
+
+    @FindBy()
+    public WebElement
+
+
+    @FindBy()
+    public WebElement
+
+
+    @FindBy()
+    public WebElement
+
+
+    @FindBy()
+    public WebElement
+
+
+    @FindBy()
+    public WebElement
+
+
+    @FindBy()
+    public WebElement
+}
+
+
