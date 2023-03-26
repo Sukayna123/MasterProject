ef6b0e4b68d5fc8f9bbb1782e797ddfc17324387
sukayna <shalashsukayna@gmail.com>
2023-03-26 13:23:32 -0400

    driver

--- src/test/java/ZUtility/Driver.java
+++ src/test/java/ZUtility/Driver.java
@@ -1,4 +1,34 @@
 package ZUtility;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class Driver {
+
+    private static WebDriver driver;
+
+    public static WebDriver getDriver(){
+        if (driver==null){
+            ChromeOptions options=new ChromeOptions();
+            options.addArguments("--remote-allow-origins=*");
+            driver=new ChromeDriver(options);
+            driver.manage().window().maximize();
+            System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY,"true");
+        }
+        return driver;
+    }
+
+    public static void quitDriver(){
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.quit();
+        driver=null;
+
+    }
 }
